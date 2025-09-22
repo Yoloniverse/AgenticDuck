1a2a87953c4c245998491f7f451ead8ba83498b0
Hojun Seo <hojuns0705@gmail.com>
2025-09-22 17:06:26 +0900

    added minimal code for planner agent and pre-hook of it to create_supervisor

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ from langgraph.checkpoint.sqlite import SqliteSaver
 from langgraph_supervisor import create_supervisor
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_ollama import ChatOllama
+from langgraph.prebuilt import create_react_agent
+from typing import TypedDict, List, Annotated
+from langchain_core.messages import BaseMessage, HumanMessage
+from langchain_core.tools import tool
+from langgraph.graph import StateGraph, END
+from langgraph.checkpoint.memory import MemorySaver
 
 # ## should change username, passcode, host, port, database names to real ones.
 # DB_URI = "postgresql://user:password@localhost:5432/dbname" 
@@ -13,6 +19,36 @@ from langchain_ollama import ChatOllama
 llm = ChatOllama(model="qwen3:8b", base_url="http://127.0.0.1:11434")
 checkpointer = SqliteSaver.from_file("langgraph_checkpoints.sqlite")
  
+
+
+planner_system_prompt_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", """
+        You are a master planner. Given the user's request, create a concise, step-by-step plan.
+        <<EXAMPLE>>
+        """),
+        ('human'), "{query}"
+    ]
+)
+
+def create_planner_agent(llm, tools) -> Runnable:
+    """Creating a planner agent"""
+    tools = [planner_func]
+    # This is a conceptual example. A real agent would be more complex.
+    llm_with_tools = llm.bind_tools(tools) 
+    llm_with_tools_chain = planner_system_prompt_template | llm_with_tools
+    return llm_with_tools_chain
+
+planner_agent = create_planner_agent(llm)
+
+
+
+
+def planner_pre_hook(state):
+    """A planner function that smartly breaks down a question of user into sub-questions"""
+
+
+
 # 이 ID는 체크포인트 파일 내에서 특정 대화 세션을 식별하는 데 사용됩니다.
 config = {"configurable": {"thread_id": "dayeon"}}
 
@@ -37,9 +73,12 @@ web_search_agent = create_react_agent(
     name="web_search_agent"
 )
 
+##https://docs.langchain.com/oss/python/langchain/short-term-memory#pre-model-hook
+##https://langchain-ai.github.io/langgraph/how-tos/create-react-agent-manage-message-history/
 supervisor = create_supervisor(
     agents=[sql_agent, rag_agent, web_search_agent],
     model=llm,
+    pre_model_hook=[planner_agent],
     prompt=(
         """너는 사용자 질문을 읽고 분석하여 필요한 기능을 선택하는 역할이야.
             [중요 원칙]
